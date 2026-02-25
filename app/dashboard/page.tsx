f98b2c3f4b133a5cd8735c990463a65925b39fca
Lazaro <lazaromiguelhernandezizquierdo@gmail.com>
2026-02-25 15:11:13 -0500

    xileffrontend commit 10

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,12 @@ import InventoryIcon from '@mui/icons-material/Inventory';
 import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 import WcIcon from '@mui/icons-material/Wc';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import CustomDataGrid from "@/components/CustomDataGrid";
+
+const rows = [
+  { id: 1, factura: "f002", cliente: "Cliente A", total: 305, fecha: "2024-01-15" },
+  { id: 2, factura: "f001", cliente: "Cliente B", total: 452, fecha: "2024-01-14" },
+];
 
 export default function dashboardPage() {
   return (
@@ -25,11 +31,27 @@ export default function dashboardPage() {
             justifyContent: 'space-between', // 👈 separa extremos
             alignItems: 'center', // 👈 centra vertical
             px: 2, // 👈 padding horizontal
+            borderBottom: '1px solid rgba(255,255,255,0.15)',// 🌫 Borde sutil
+            boxShadow: '0 4px 20px rgba(0,0,0,0.08)',// 💡 Sombra suave
           }}>
           <Typography variant="h5" color='white' sx={{ ml: 2 }}>
             Dashboard General
           </Typography>
-          <Button variant="contained" startIcon={<RestartAltIcon />}>
+          <Button
+            variant="contained"
+            startIcon={<RestartAltIcon className="rotate-icon" />}
+            sx={{
+              transition: 'all 0.3s ease',
+
+              '&:hover .rotate-icon': {
+                transform: 'rotate(180deg) scale(1.1)',
+              },
+
+              '& .rotate-icon': {
+                transition: 'transform 0.3s ease',
+              },
+            }}
+          >
             Actualizar
           </Button>
         </Box>
@@ -56,6 +78,23 @@ export default function dashboardPage() {
             <CardContent>
               <Typography variant="h6"><WcIcon fontSize='large' sx={{ mr: 1 }} />Empleados:</Typography>
               <Typography variant="h4" color='primary' textAlign={'center'}>6</Typography>
+            </CardContent>
+          </Card>
+        </Box>
+        <Box ml={2}>
+          <Card sx={{ width: 'auto', display: 'inline-flex', m: 2 }}>
+            <CardContent>
+              <CustomDataGrid
+                title="Ventas Recientes"
+                rows={rows}
+                getRowId={(row) => row.id}
+                columns={[
+                  { field: "factura", headerName: "Factura" },
+                  { field: "cliente", headerName: "Cliente", numeric: true },
+                  { field: "total", headerName: "Total", numeric: true },
+                  { field: "fecha", headerName: "Fecha", numeric: true },
+                ]}
+              />
             </CardContent>
           </Card>
         </Box>
